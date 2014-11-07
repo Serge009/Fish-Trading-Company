6672a7942074f0db9aa4d2156feb473a3af89416
Serge009 <serge.naumovych@gmail.com>
2014-11-07 11:00:04 +0200

    remove hibernate-search

--- src/main/java/org/serge009/fishtrading/core/dao/ProductDaoImpl.java
+++ src/main/java/org/serge009/fishtrading/core/dao/ProductDaoImpl.java
@@ -1,14 +1,12 @@
 package org.serge009.fishtrading.core.dao;
 
-import org.apache.lucene.search.Query;
-import org.hibernate.search.jpa.FullTextEntityManager;
-import org.hibernate.search.query.dsl.QueryBuilder;
 import org.serge009.fishtrading.core.entity.Product;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -29,25 +27,6 @@ public class ProductDaoImpl implements ProductDao {
     @Override
     @Transactional(readOnly = true)
     public List<Product> findByKeyword(String keyword) {
-        FullTextEntityManager fullTextEntityManager =
-                org.hibernate.search.jpa.Search.getFullTextEntityManager(em);
-        // create native Lucene query unsing the query DSL
-        // alternatively you can write the Lucene query using the Lucene query parser
-        // or the Lucene programmatic API. The Hibernate Search DSL is recommended though
-        QueryBuilder qb = fullTextEntityManager.getSearchFactory()
-                .buildQueryBuilder().forEntity(Product.class).get();
-
-        Query luceneQuery = qb
-                .keyword()
-                .onFields("name")
-                .matching(keyword)
-                .createQuery();
-
-        // wrap Lucene query in a javax.persistence.Query
-        javax.persistence.Query jpaQuery =
-                fullTextEntityManager.createFullTextQuery(luceneQuery, Product.class);
-
-        // execute search
-        return jpaQuery.getResultList();
+        return Collections.emptyList();
     }
 }
